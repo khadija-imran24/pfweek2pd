4b0e3bf8ccf3373cba4c46cc666ad728b3110988
khadija-imran24 <121511566+khadija-imran24@users.noreply.github.com>
2023-01-20 14:11:49 +0500

    pfweek2pd

---
+++ task10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"----------------------------"<<endl;
+cout<<" "<<endl;
+cout<<"     o     ^__^"<<endl;
+cout<<" "<<endl;
+cout<<"        o ( oo )\\_______________"<<endl;
+cout<<" "<<endl;
+cout<<"          ( __ )\\                )/\\/"<<endl;
+cout<<" "<<endl;
+cout<<"                 ||------------w |"<<endl;
+cout<<" "<<endl;
+cout<<"                 ||             ||"<<endl;
+}
---
+++ task9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system ("color 57");
+cout<<".....                ooooo    ooooo             ............"<<endl;
+cout<<"......              o     o  o     o            ..........."<<endl;
+cout<<".......                ooo   o     o            .........."<<endl;
+cout<<"........             oo      o     o            ........."<<endl;
+cout<<" ........           ooooooo   ooooo  th         ........"<<endl;
+cout<<"  ........                                      ......."<<endl;
+cout<<"    .......        ----------------------       ......"<<endl;
+cout<<"      ......         c  e  n  t  u  r  y        ....."<<endl;
+cout<<"         .....     ----------------------       ...."<<endl;
+cout<<"           ...      ooooo ooooo o   o ooooo     ..."<<endl;
+cout<<"             **       o   o      o o    o       **"<<endl;
+cout<<"            _||_      o   oooo    o     o      _||_"<<endl;
+cout<<"           |    |     o   o      o o    o     |    |"<<endl;
+cout<<"___________|____|_____o   ooooo o   o   o_____|____|"<<endl;
+
+}
